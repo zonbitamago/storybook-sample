2e5450fb3840a23754d777a8be8bbf2fd82ffec4
kazuma takiuchi <basstrombone413@gmail.com>
2018-04-17 04:52:31 +0900

    fix import story

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,12 @@
 import { configure } from "@storybook/react";
+const req = require.context("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
   // require("../stories/index.js");
   // You can require as many stories as you need.
-  require("../src/components/Button/Button.stories.js");
+  // require("../src/components/Button/Button.stories.js");
+
+  req.keys().forEach(filename => req(filename));
 }
 
 configure(loadStories, module);
